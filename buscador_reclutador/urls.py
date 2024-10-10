3545309da4dc6bcba53a39fca6e5e4580c4a721e
afprietol <afprietol@eafit.edu.co>
2024-10-10 15:00:20 -0500

    creacion de visualizacion videos de aspirantes para los reclutadores

--- buscador_reclutador/urls.py
+++ buscador_reclutador/urls.py
@@ -3,4 +3,6 @@ from . import views  # Importas las vistas de tu aplicación
 
 urlpatterns = [
     path('perfiles_aspirantes/', views.perfiles_aspirantes, name='perfiles_aspirantes'),
+    path('videos_aspirantes_disponibles/', views.videos_aspirantes_disponibles, name='videos_aspirantes_disponibles'),
+
 ]
--- buscador_reclutador/views.py
+++ buscador_reclutador/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from users.models import Aspirante
+from buscador_aspirante.models import Video_post
 # Create your views here.
 
 def perfiles_aspirantes(request):
@@ -7,3 +8,7 @@ def perfiles_aspirantes(request):
     aspirantes = Aspirante.objects.all()
     return render(request, 'perfiles_aspirantes.html', {'aspirantes': aspirantes})
 
+
+def videos_aspirantes_disponibles(request):
+    videos = Video_post.objects.all()
+    return render(request, 'videos_aspirantes_disponibles.html', {'videos': videos})
